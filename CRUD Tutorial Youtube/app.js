32df4f7c7532c23fc6b74018408b438164db74d5
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2022-07-12 20:55:51 +0800

    Add files via upload

---
+++ CRUD Tutorial Youtube/app.js
@@ -0,0 +1,58 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+//Functions
+
+function addTodo(event){
+    //Prevent form from submitting
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //Create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check-circle"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //CHECK TRASH BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
+    todoInput.value = "";
+}
+
+function deleteCheck(e){
+	const item = e.target;
+	//DELETE TODO
+	if(item.classList[0] === "trash-btn"){
+		const todo = item.parentElement;
+		///Animation
+		todo.classList.toggle("fail");
+		todo.addEventListener("transitionend", function(){
+			todo.remove();
+		});
+		
+	}
+	//CHECK MARK
+	if(item.classList[0] === "complete-btn"){
+		const todo = item.parentElement;
+		todo.classList.toggle("completed");
+	
+	}
+}
+
